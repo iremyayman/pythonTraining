dd5702d622c1968e5cf93709358f66d836c27ba8
Irem <iremyayman12@gmail.com>
2021-10-12 18:54:34 +0300

    first commit

---
+++ SimpleStockPrice.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 13:28:39 2021
+
+@author: irem
+"""
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+         # Simple Stock Price App
+         
+         Shown are the stock **closing price** and **volume** of Google
+         
+         """)
+         
+tickerSymbol='GOOGL'
+tickerData=yf.Ticker(tickerSymbol)
+tickerDf=tickerData.history(period='1d',start='2010-5-31',end='2020-5-31')
+
+st.write("""
+         ## Closing Price
+         """)
+st.line_chart(tickerDf.Close)
+
+st.write("""
+         ## Volume Price
+         """)
+
+st.line_chart(tickerDf.Volume)         
---
+++ history.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Oct 11 13:03:57 2021)---
+runfile('C:/Users/irem/.spyder-py3/temp.py', wdir='C:/Users/irem/.spyder-py3')
+runcell(0, 'C:/Users/irem/.spyder-py3/temp.py')
+runfile('C:/Users/irem/.spyder-py3/SimpleStockPrice.py', wdir='C:/Users/irem/.spyder-py3')
+
+## ---(Tue Oct 12 15:36:43 2021)---
+!git add "SimpleStockPrice.py"
+!git add "SimpleStockPrice.py"
+!git commit -m "SimpleStockPrice"
+!git push origin master
+!git add "PythonExercisesForDataScience.py"
+!git commit -m "My commit"
+!git push origin master
